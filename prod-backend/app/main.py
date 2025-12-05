37ef1619b559df891c70d8e2d1a493198dddb04c
DebboyRohan <rohankumarpandey234@gmail.com>
2025-12-05 12:45:41 +0530

    Added /test

--- prod-backend/app/main.py
+++ prod-backend/app/main.py
@@ -1,15 +1,17 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from prometheus_fastapi_instrumentator import Instrumentator
+from requests import Session
 import uvicorn
 import logging
 
 from app.core.config import settings
 from app.api.v1.router import api_router
 from app.db.models import Base
-from app.core.database import engine
+from app.core.database import engine, get_db
 from app.core.logging_middleware import TimeLoggingMiddleware
-
+from app.api.v1.endpoints.test_pipeline import test_pipeline
+from app.schemas.test_structure import TestPipelineRequest, TestPipelineResponse
 # Configure basic logging
 logging.basicConfig(
     level=logging.INFO,
@@ -31,12 +33,7 @@ app = FastAPI(
 app.add_middleware(TimeLoggingMiddleware)
 
 # CORS
-origins = [
-    "http://localhost:3000",
-    "http://localhost:8000",
-    # Add your EC2 public IP or domain here later
-    "*" # For dev, restrict in prod
-]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -57,6 +54,11 @@ app.include_router(api_router, prefix=settings.API_V1_STR)
 @app.get("/")
 def read_root():
     return {"status": "healthy", "project": settings.PROJECT_NAME}
+
+@app.post("/test", response_model=TestPipelineResponse, summary="End-to-End Pipeline Test")
+async def test(payload: TestPipelineRequest, db: Session = Depends(get_db)):
+    return test_pipeline(payload,db)
+
 
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
